21efd959dae531bbbd102dd0e3b6abe5dd6ca2bb
EduardoFL <floreslenguaeduardo@gmail.com>
2023-06-08 22:23:15 -0500

    buscar producto por nombre

--- src/main/java/com/company/inventory/dao/IProductDAO.java
+++ src/main/java/com/company/inventory/dao/IProductDAO.java
@@ -1,5 +1,8 @@
 package com.company.inventory.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.company.inventory.model.Product;
 
 @Repository
 public interface IProductDAO extends CrudRepository<Product, Long>{
-
+	
+	@Query("SELECT p FROM Product p WHERE p.name LIKE %?1%")
+	List<Product> findByNameLike(String name);
+	
+	List<Product> findByNameContainingIgnoreCase(String name);
 }
