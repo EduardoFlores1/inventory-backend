04d8d5b6797b004d23c2c7c59576bfe8b1167cce
EduardoFL <floreslenguaeduardo@gmail.com>
2023-06-08 22:37:07 -0500

    eliminar producto por id

--- src/main/java/com/company/inventory/services/ProductServiceImpl.java
+++ src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -142,4 +142,24 @@ public class ProductServiceImpl implements IProductService{
 		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
 	}
 
+	@Override
+	@Transactional
+	public ResponseEntity<ProductResponseRest> deleteById(Long id) {
+		ProductResponseRest response = new ProductResponseRest();
+		
+		try {
+			
+			// delete product by id
+			productDAO.deleteById(id);
+			response.setMetadata("Respuesta ok", "00", "Producto Eliminado");
+			
+		} catch (Exception e) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta nok", "-1", "Error al eliminar producto");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+	}
+
 }
